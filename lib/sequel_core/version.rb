e9499879ae7b834314a65db590b90629a58c4ccd
Colin Casey <casey.colin@gmail.com>
2009-02-26 23:45:21 -0400

    switched to jeweler to manage gem build and release

--- lib/sequel_core/version.rb
+++ lib/sequel_core/version.rb
@@ -1,11 +1,15 @@
-module Sequel
-  MAJOR = 2
-  MINOR = 10
-  TINY  = 0
-  
-  VERSION = [MAJOR, MINOR, TINY].join('.')
-  
+require 'yaml'
+
+module Sequel  
   def self.version
-    VERSION
+    @version ||= load_from_version_file
+  end
+
+  def self.load_from_version_file
+    version_config = YAML.load_file(File.join(File.dirname(__FILE__), '..', '..', 'VERSION.yml'))
+    major = version_config[:major]
+    minor = version_config[:minor]
+    patch = version_config[:patch]
+    "#{major}.#{minor}.#{patch}"
   end
 end
